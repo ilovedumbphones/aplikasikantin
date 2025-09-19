a7c797caecc03bbe19e2d0484c163b890e0fbac9
ilovedumbphones <102640192+ilovedumbphones@users.noreply.github.com>
2025-09-19 15:42:13 +0700

    Update tab.js

--- tab.js
+++ tab.js
@@ -1,13 +1,20 @@
-function opentab(evt, ce) {
-  var i, x, tablinks;
-  x = document.getElementsByClassName("tabss");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+function openCity(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
-  tablinks = document.getElementsByClassName("tablink");
-  for (i = 0; i < x.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" red", "");
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  document.getElementById(ce).style.display = "block";
-  evt.currentTarget.className += " red";
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
 }
