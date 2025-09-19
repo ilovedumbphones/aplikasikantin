51b98cbd3fef0695dd4f8a4f6e16ac81b9e46f27
ilovedumbphones <102640192+ilovedumbphones@users.noreply.github.com>
2025-09-19 15:32:40 +0700

    Update tab.js

--- tab.js
+++ tab.js
@@ -6,8 +6,8 @@ function opentab(Nme) {
   }
   tablinks = document.getElementsByClassName("tablink");
   for (i = 0; i < x.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" w3-green", "");
+    tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
   }
   document.getElementById(Nme).style.display = "block";
-  evt.currentTarget.className += " w3-green";
+  evt.currentTarget.className += " w3-red";
 }
